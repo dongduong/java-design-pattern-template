ddcf5c9b2d18021cb08d5bafd9a3049cecb40d06
Dong Duong <dhdong1510@gmail.com>
2016-09-12 07:46:34 +0700

    java doc

--- creational-design-pattern/src/com/dongduong/designpatern/abstractfactory/TestAbstractFactory.java
+++ creational-design-pattern/src/com/dongduong/designpatern/abstractfactory/TestAbstractFactory.java
@@ -1,3 +1,11 @@
+/**
+ * Abstract Factory pattern is similar to Factory design pattern and it's factory of factories. If you are familiar with factory
+ * design pattern in java, you will notice that we have a single Factory class that returns the different sub-classes bases on the
+ * input provided and factory class uses if-else or switch statement to achieve this. In Abstract Factory pattern, we get rid of
+ * if-else block and have a factory class for each sub-class and then an Abstract Factory class will return the sub-class based on
+ * the input factory class. Check out Abstract Factory Pattern to know how to implement this pattern with example program.
+ */
+
 package com.dongduong.designpatern.abstractfactory;
 
 public class TestAbstractFactory {
